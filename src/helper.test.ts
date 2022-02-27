e751056c41234400aecb7196e8fab02a7b13d1be
faruqneo <faruq_khan@yahoo.com>
2022-02-27 14:13:47 +0530

    added test caes

--- src/helper.test.ts
+++ src/helper.test.ts
@@ -1,7 +1,7 @@
 import { random } from "./helper";
 
 test('should return 4 digit unique number', () => {
-    const unique = random;
+    const unique = random(0, 4);
     expect(unique).not.toBeNull();
     expect(unique).not.toBeNaN();
   });
